87b7deacd3774125eb1c798f295403d9cea27de9
HovannesPiloshian <hovannespiloshian24@gmail.com>
2025-03-28 17:36:13 +0400

    Small app.cpp

--- Small app.cpp
+++ Small app.cpp
@@ -3,12 +3,12 @@
 #include <comdef.h>
 #include <Wbemidl.h>
 
-#pragma comment(lib, "wbemuuid.lib")
+
 
 void getPrinterAttributes() {
     HRESULT hres;
 
-    // Step 1: Initialize COM Library
+  
     hres = CoInitializeEx(0, COINIT_MULTITHREADED);
     if (FAILED(hres)) {
         std::cerr << "COM initialization failed. Error: 0x"
@@ -16,7 +16,7 @@ void getPrinterAttributes() {
         return;
     }
 
-    // Step 2: Initialize COM Security
+    
     hres = CoInitializeSecurity(
         NULL, -1, NULL, NULL,
         RPC_C_AUTHN_LEVEL_DEFAULT,
@@ -30,7 +30,6 @@ void getPrinterAttributes() {
         return;
     }
 
-    // Step 3: Get WMI Locator
     IWbemLocator* pLoc = NULL;
     hres = CoCreateInstance(
         CLSID_WbemLocator, 0, CLSCTX_INPROC_SERVER,
@@ -43,7 +42,7 @@ void getPrinterAttributes() {
         return;
     }
 
-    // Step 4: Connect to WMI Service
+    
     IWbemServices* pSvc = NULL;
     hres = pLoc->ConnectServer(
         _bstr_t(L"ROOT\\CIMV2"), NULL, NULL, 0, NULL, 0, 0, &pSvc
@@ -56,7 +55,7 @@ void getPrinterAttributes() {
         return;
     }
 
-    // Step 5: Set security levels
+  
     hres = CoSetProxyBlanket(
         pSvc, RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, NULL,
         RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE,
@@ -71,7 +70,7 @@ void getPrinterAttributes() {
         return;
     }
 
-    // Step 6: Execute WMI Query to Get Printer Information
+    
     IEnumWbemClassObject* pEnumerator = NULL;
     hres = pSvc->ExecQuery(
         bstr_t("WQL"),
@@ -89,35 +88,35 @@ void getPrinterAttributes() {
         return;
     }
 
-    // Step 7: Process Results
+    
     IWbemClassObject* pclsObj = NULL;
     ULONG uReturn = 0;
     while (pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn) == S_OK) {
         VARIANT vtProp;
 
-        // Printer Name
+       
         pclsObj->Get(L"Name", 0, &vtProp, 0, 0);
         std::wcout << L"\nPrinter Name: " << vtProp.bstrVal << std::endl;
         VariantClear(&vtProp);
 
-        // Printer Status
+       
         pclsObj->Get(L"PrinterStatus", 0, &vtProp, 0, 0);
         std::wcout << L"Printer Status: " << vtProp.uintVal << std::endl;
         VariantClear(&vtProp);
 
-        // Location
+       
         pclsObj->Get(L"Location", 0, &vtProp, 0, 0);
         if (vtProp.vt != VT_NULL) {
             std::wcout << L"Location: " << vtProp.bstrVal << std::endl;
         }
         VariantClear(&vtProp);
 
-        // Driver Name
+       
         pclsObj->Get(L"DriverName", 0, &vtProp, 0, 0);
         std::wcout << L"Driver Name: " << vtProp.bstrVal << std::endl;
         VariantClear(&vtProp);
 
-        // Default Printer
+       
         pclsObj->Get(L"Default", 0, &vtProp, 0, 0);
         std::wcout << L"Default Printer: " << (vtProp.boolVal ? L"Yes" : L"No") << std::endl;
         VariantClear(&vtProp);
@@ -125,7 +124,7 @@ void getPrinterAttributes() {
         pclsObj->Release();
     }
 
-    // Step 8: Cleanup
+    
     pSvc->Release();
     pLoc->Release();
     pEnumerator->Release();
